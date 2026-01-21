5d731fcb648bbaefef70996aeeedb0eaaaca3d5c
Saranyalahari <saranyalaharienuga@gmail.com>
2026-01-21 21:00:45 +0530

    save

---
+++ Task 1/hello_world.py
@@ -0,0 +1,32 @@
+# # hello_world.py
+#
+# # Store your name in a variable
+# name = "Saranya Lahari"
+#
+# # Store your internship role in a variable
+# role = "AI Intern"
+#
+# # Store today's date in a variable
+# date = "15-01-2026"
+#
+# # Print the details
+# print("Name:", name)
+# print("Internship Role:", role)
+# print("Date:", date)
+
+# hello_world.py
+
+# Take name input from the user
+name = input("Enter your name: ")
+
+# Take internship role input from the user
+role = input("Enter your internship role: ")
+
+# Take date input from the user
+date = input("Enter today's date (DD-MM-YYYY): ")
+
+# Display the entered details
+print("\n--- Details Entered ---")
+print("Name:", name)
+print("Internship Role:", role)
+print("Date:", date)
